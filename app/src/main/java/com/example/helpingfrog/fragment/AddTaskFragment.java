4b0833ca1b88b2d1850aa5e1232a9fa61db57dfe
Mitya <you@example.com>
2022-05-19 01:26:10 +0300

    commit

---
+++ app/src/main/java/com/example/helpingfrog/fragment/AddTaskFragment.java
@@ -0,0 +1,26 @@
+package com.example.helpingfrog.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.helpingfrog.R;
+
+import java.util.zip.Inflater;
+
+
+public class AddTaskFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_add_task, container, false);
+
+        return view;
+    }
+}
---
+++ app/src/main/java/com/example/helpingfrog/fragment/ChangeTaskFragment.java
@@ -0,0 +1,23 @@
+package com.example.helpingfrog.fragment;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.helpingfrog.R;
+
+public class ChangeTaskFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View view = inflater.inflate(R.layout.fragment_change_task, container, false);
+
+        return view;
+    }
+}
